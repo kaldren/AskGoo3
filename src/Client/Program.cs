ba7d3a9e130630503d8a781d9296e895a7e7614b
kaldren <drenski666@gmail.com>
2019-03-02 17:25:38 +0200

    Added console client to test identityserver

---
+++ src/Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel;
+
+namespace Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync()
+        {
+            var client = new HttpClient();
+
+            var response = await client.RequestTokenAsync(new TokenRequest
+            {
+                Address = "https://localhost:5001/connect/token",
+                GrantType = OidcConstants.GrantTypes.ClientCredentials,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+
+                Parameters =
+                {
+                    { "scope", "api1" }
+                }
+            });
+
+            if (response.IsError)
+            {
+                throw new Exception(response.Error);
+            }
+
+            var token = response.AccessToken;
+
+            Console.WriteLine(response.Json);
+            Console.ReadLine();
+        }
+    }
+}
--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using IdentityServer4.Models;
 
-namespace IdentityServer
+namespace AskGoo3.IdentityServer
 {
     public class Config
     {
