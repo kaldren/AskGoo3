686140f2ac0b460068704d0a29e40ea876c72644
kaldren <drenski666@gmail.com>
2019-03-03 11:00:26 +0200

    Added db context

---
+++ src/Infrastructure/Data/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace AskGoo3.Infrastructure.Data
+{
+    public sealed class DatabaseContext :  DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+        }
+    }
+}
