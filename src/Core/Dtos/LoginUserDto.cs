2e7a2b5d7cbfd3947339faefb94b28fe5132bf8e
kaldren <drenski666@gmail.com>
2019-03-03 12:21:26 +0200

    LoginUserDto

---
+++ src/Core/Dtos/LoginUserDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AskGoo3.Core.Dtos
+{
+    public class LoginUserDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
