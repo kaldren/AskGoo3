3bc633588f196835ed19b576f3e2a1c51efc4ae3
kaldren <drenski666@gmail.com>
2019-03-03 13:58:14 +0200

    Added UserRepository

---
+++ src/Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using AskGoo3.Core.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AskGoo3.Core.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> SignInUserAsync(string username, string password);
+    }
+}
---
+++ src/Infrastructure/Implementations/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AskGoo3.Core.Entities.User;
+using AskGoo3.Core.Interfaces;
+using AskGoo3.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AskGoo3.Infrastructure.Implementations
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public UserRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> SignInUserAsync(string username, string password)
+        {
+            return await _context.Users.SingleOrDefaultAsync(x => x.Username == username && x.Password == password);
+        }
+    }
+}
