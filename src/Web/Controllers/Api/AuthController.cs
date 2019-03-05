e919e72741b1d7218facda4bcfab19e85ccb35e9
kaldren <drenski666@gmail.com>
2019-03-05 17:56:17 +0200

    Removed Web

---
+++ src/Web/Controllers/Api/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using AskGoo3.Core.Dtos;
+using AskGoo3.Services.Api;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AskGoo3.Web.Controllers.Api
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class AuthController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Authenticate([FromBody] LoginUserDto dto)
+        {
+            var user = await _userService.AuthenticateAsync(dto.Username, dto.Password);
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid Username / Password");
+            }
+
+            return Ok(user);
+        }
+
+    }
+}
