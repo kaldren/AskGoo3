37d09c504df1cbdc46824ae9a0a33da9ba2fc96c
kaldren <drenski666@gmail.com>
2019-03-05 21:15:49 +0200

    Login form wip

--- src/Web/ClientApp/src/app/main/login/login.component.ts
+++ src/Web/ClientApp/src/app/main/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from 'src/app/_models/login';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  model = new Login('kdrenski', 'kdrenski');
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log('submit clicked!');
   }
 
 }
