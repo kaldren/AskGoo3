f78c1e0bf44b600fb77f556b4182ab3f3ebb8eb5
kaldren <drenski666@gmail.com>
2019-03-05 13:06:16 +0200

    Revert "Removed Web"
    This reverts commit 9feb718f0f00d41e0fa9929e3821c281e74868ca.

---
+++ src/Web/ClientApp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main/main.component';
+import { LoginComponent } from './main/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
