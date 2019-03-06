4e0aba174024438f2d0599f6281055171e29f5dd
kaldren <drenski666@gmail.com>
2019-03-06 23:25:40 +0200

    signin wip

--- src/Web/ClientApp/src/app/main/signin/signin.component.ts
+++ src/Web/ClientApp/src/app/main/signin/signin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SignIn } from 'src/app/_models/signin';
 import { AuthService } from 'src/app/_services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -12,7 +13,7 @@ export class SignInComponent implements OnInit {
 
   signInUser: SignIn;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
     this.signInUser  = {
@@ -26,6 +27,7 @@ export class SignInComponent implements OnInit {
       .subscribe((data) => {
         if (data['token']) {
           localStorage.setItem('token', data['token']);
+          this.router.navigate(['dashboard/home']);
         } else {
         console.log('Invalid credentials.')
         }
