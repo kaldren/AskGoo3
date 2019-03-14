9106c3aa7a03789b2cf3e22d8c03b1a93f4850e5
kaldren <drenski666@gmail.com>
2019-03-14 21:01:58 +0200

    MapperConfig

---
+++ src/Web/MapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AskGoo3.Core.Dtos;
+using AskGoo3.Core.Entities.MessageAggregate;
+using AskGoo3.Core.Entities.UserAggregate;
+
+namespace AskGoo3.Web
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<Message, MessageDto>();
+        }
+    }
+}
