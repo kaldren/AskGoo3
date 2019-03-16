803394822ee9f8e4051cc5be411b2bacc42a9d34
kaldren <drenski666@gmail.com>
2019-03-16 12:46:41 +0200

    Show content and id

--- src/Web/ClientApp/src/app/main/dashboard/messages/message/message.component.ts
+++ src/Web/ClientApp/src/app/main/dashboard/messages/message/message.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MessageService } from 'src/app/_services/message/message.service';
+import { Message } from 'src/app/_models/message';
 
 @Component({
   selector: 'app-message',
@@ -10,13 +12,23 @@ export class MessageComponent implements OnInit, OnDestroy {
 
   id: number;
   content: string;
+  message: any;
   private sub: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private messageService: MessageService) { }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
-      this.id = +params.id;
+      this.getSingleMessageByUserId(+params.id);
+    });
+
+  }
+
+  getSingleMessageByUserId(id: number) {
+    this.messageService.getSingleMessageById(id)
+    .subscribe((data) => {
+      this.message = data;
     });
   }
 
