a34348683dff96426a80801d67d588efc81de916
kaldren <drenski666@gmail.com>
2019-03-17 14:18:04 +0200

    Profile wip

--- src/Web/ClientApp/src/app/main/dashboard/profile/profile.component.ts
+++ src/Web/ClientApp/src/app/main/dashboard/profile/profile.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.less']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
+  id: number;
+  content: string;
+  message: any;
+  private sub: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      // this.getSingleMessageByUserId(+params.id);
+      this.id = +params.id;
+    });
+  }
+
+  getSingleProfileById(id: number) {
+
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
---
+++ src/Web/Controllers/Api/ProfileController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AskGoo3.Web.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ProfileController : Controller
+    {
+    }
+}
